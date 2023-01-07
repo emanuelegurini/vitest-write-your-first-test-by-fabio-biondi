85f8ff98403bbd819329d3b0d73e0f73328db643
emanuelegurini <emanuele.gurini89@gmail.com>
2023-01-07 22:36:31 +0100

    feat: :sparkles: Code Coverage

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], // <==
-    setupFiles: "./setupTests.ts", //
+    setupFiles: "./setupTests.ts", // <==
+    coverage: {
+      provider: "istanbul", // <==
+    },
   },
   plugins: [react(), checker({ typescript: true })],
 });
